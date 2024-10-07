75663fe69a45445c05316ddac053ac6b8f5f1e01
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2024-10-07 19:25:14 +0300

    version

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ namespace JailbirdChargeWarner
 
         public override Version RequiredExiledVersion { get; } = new Version(8, 12, 0);
 
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 1, 0);
         private Harmony harmony;
         public override void OnEnabled()
         {
